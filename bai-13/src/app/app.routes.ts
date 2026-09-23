a2dcb5f61497ea7f9444ff673bcf412a1a80c3c9
linhtntk24411-alt <linhtntk24411@st.uel.edu.vn>
2026-09-23 20:28:10 +0000

    hoan thanh bai-13

--- bai-13/src/app/app.routes.ts
+++ bai-13/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ServiceProductImageEventComponent } from './service-product-image-event
 import { ServiceProductImageEventDetailComponent } from './service-product-image-event-detail/service-product-image-event-detail';
 
 export const routes: Routes = [
+  { path: '', component: ServiceProductImageEventComponent },
   { path: 'service-product-image-event', component: ServiceProductImageEventComponent },
   { path: 'service-product-image-event/:id', component: ServiceProductImageEventDetailComponent },
 ];
